afaff930c4f25343922a8b0bd8a71bce87155b82
CarlosSanchez22 <90216758+CarlosSanchez22@users.noreply.github.com>
2025-03-24 17:15:40 -0700

    Create db.js

---
+++ server/db.js
@@ -0,0 +1,25 @@
+import {createPool} from 'mysql2/promise';
+
+export const pool = new createPool({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '', 
+    database: 'VetGuardianDB'
+});
+
+async function testConnection() {
+    try {
+        const connection = await pool.getConnection();
+        console.log('Conexión exitosa a la base de datos.');
+        connection.release();
+    } catch (err) {
+        console.error('Error al conectar a la base de datos:', err.message);
+        console.error('Código de error:', err.code);
+    }
+}
+ww
+testConnection();
+
+
+
